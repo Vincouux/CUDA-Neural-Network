76fe6bfa15ab511cc23c651e6beba6990b5d2143
Vincent <vincent1.riviere@epita.fr>
2020-01-08 21:47:36 +0100

    [MAIN] Main updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "Model/model.hpp"
+#include "Layer/layer.hpp"
+#include "Activation/activation.hpp"
 
-int main(int argc, char** argv) {
+int main() {
     Model model = Model();
-    model.add(Layer(2, Layer::Input));
-    model.add(Layer(5, Layer::Dense, ActivationFunction::Sigmoid));
-    model.add(Layer(1, Layer::Dense, ActivationFunction::Sigmoid));
+    model.add(Layer(3, Input, Sigmoid));
+    model.add(Layer(2, Dense, Sigmoid));
+    model.add(Layer(1, Dense, Sigmoid));
     model.compile();
+    model.summary();
     return 0;
 }
