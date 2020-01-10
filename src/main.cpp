57aefa78284a16fca786a01f5995139d6adc1a2e
Vincent <vincent1.riviere@epita.fr>
2020-01-10 16:47:06 +0100

    [MAIN] Main updated

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,16 @@
 
 int main() {
     Model model = Model();
-    model.add(new Input(3));
-    model.add(new Dense(2, Sigmoid));
+    model.add(new Input(2));
+    model.add(new Dense(3, Sigmoid));
     model.add(new Dense(1, Sigmoid));
     model.compile();
     model.summary();
+
+    Matrix<float> X = Matrix<float>({{0., 1.}, {1., 0.}, {1., 1.}});
+    Matrix<float> Y = Matrix<float>({{1.}, {1.}, {0.}});
+
+    //model.fit(X, Y);
+
     return 0;
 }
