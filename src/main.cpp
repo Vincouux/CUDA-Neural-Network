3f7b954e55b55e5705aa80a94e4a6ba630dff8d3
Vincent <vincent1.riviere@epita.fr>
2020-01-11 19:10:38 +0100

    [MAIN] Main updated

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main() {
     Matrix<float> X = Matrix<float>({{0., 1.}, {1., 0.}, {1., 1.}});
     Matrix<float> Y = Matrix<float>({{1.}, {1.}, {0.}});
 
-    //model.fit(X, Y);
+    model.fit(X, Y);
 
     return 0;
 }
