21559aa96a18e00c267e1b982c1f9978ff14f3f8
Vincent <vincent1.riviere@epita.fr>
2020-01-14 21:50:44 +0100

    [MISC] Updated main & test

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Layer/input.hpp"
 #include "Layer/dense.hpp"
 #include "Activation/activation.hpp"
+#include "Matrix/matrix.hpp"
 
 int main() {
 
@@ -16,7 +17,7 @@ int main() {
         model.summary();
         Matrix<float> X = Matrix<float>({{0., 1.}, {1., 0.}, {0., 0.}, {1., 1.}});
         Matrix<float> Y = Matrix<float>({{1.}, {1.}, {0.}, {0.}});
-        model.fit(X, Y, 2000, 0.1f, 1, true);
+        model.fit(X, Y, 2000, 0.1f, false);
         Matrix<float> P = model.predict(X);
         Y.transpose().display();
         P.transpose().display();
@@ -29,11 +30,13 @@ int main() {
         model.summary();
         Matrix<float> X = Matrix<float>("data/x_test.csv") / 255.f;
         Matrix<float> Y = Matrix<float>("data/y_test.csv");
-        model.fit(X, Y, 1, 0.01f, 1, true);
-        Matrix<float> P = model.predict(X);
+        model.fit(X, Y, 1, 0.01f, true, true);
+        Matrix<float> X2 = Matrix<float>("data/x_train.csv") / 255.f;
+        Matrix<float> Y2 = Matrix<float>("data/y_train.csv");
+        Matrix<float> P = model.predict(X2);
         for (unsigned i = 0; i < 20; i++) {
             std::cout << "Expect:" << std::endl;
-            Y.getLine(i).display();
+            Y2.getLine(i).display();
             std::cout << "Got" << std::endl;
             P.getLine(i).display();
             std::cout << std::endl << "------------------" << std::endl;
